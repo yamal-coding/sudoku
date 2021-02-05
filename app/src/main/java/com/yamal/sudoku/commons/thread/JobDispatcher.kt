efaae46654b4108469f563a89c49c30dc6dc576e
yamal <yamal.coding@gmail.com>
2021-02-05 12:36:13 +0100

    delete unused file

--- app/src/main/java/com/yamal/sudoku/commons/thread/JobDispatcher.kt
+++
@@ -1,6 +0,0 @@
-package com.yamal.sudoku.commons.thread
-
-interface JobDispatcher { // TODO replace with coroutines
-    fun runOnUIThread(block: () -> Unit)
-    fun runOnDiskThread(block: () -> Unit)
-}
