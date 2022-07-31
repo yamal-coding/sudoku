bf7b23a3dc1d1e124143b6aee7f4f8405469820e
yamal <yamal.coding@gmail.com>
2022-07-31 21:28:12 +0200

    fix detekt

--- app/src/main/java/com/yamal/sudoku/game/ui/NumberPad.kt
+++ app/src/main/java/com/yamal/sudoku/game/ui/NumberPad.kt
@@ -3,7 +3,6 @@ package com.yamal.sudoku.game.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Icon
