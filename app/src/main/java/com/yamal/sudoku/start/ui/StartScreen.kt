b5e7bd8be3a33460ebe85ab4d1e65d18888ee139
yamal <yamal.coding@gmail.com>
2022-08-26 12:36:04 +0200

    start screen buttons refactor

--- app/src/main/java/com/yamal/sudoku/start/ui/StartScreen.kt
+++ app/src/main/java/com/yamal/sudoku/start/ui/StartScreen.kt
@@ -1,10 +1,16 @@
 package com.yamal.sudoku.start.ui
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -17,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yamal.sudoku.R
 import com.yamal.sudoku.commons.ui.theme.SudokuTheme
+import com.yamal.sudoku.start.ui.viewmodel.StartScreenState
 import com.yamal.sudoku.start.ui.viewmodel.StartViewModel
 
 @Composable
@@ -27,17 +34,22 @@ fun StartScreen(
     onStartNewMediumGame: () -> Unit,
     onStartNewHardGame: () -> Unit,
 ) {
-    val shouldShowContinueButton by remember {
-        viewModel.shouldShowContinueButton
-    }.collectAsState(initial = false)
+    val startScreenState by remember {
+        viewModel.startScreenState
+    }.collectAsState(initial = StartScreenState.Idle)
 
-    StartScreen(
-        shouldShowContinueButton = shouldShowContinueButton,
-        onContinueGame = onContinueGame,
-        onStartNewEasyGame = onStartNewEasyGame,
-        onStartNewMediumGame = onStartNewMediumGame,
-        onStartNewHardGame = onStartNewHardGame
-    )
+    when (startScreenState) {
+        is StartScreenState.Idle -> {}
+        is StartScreenState.Ready -> {
+            StartScreen(
+                shouldShowContinueButton = (startScreenState as StartScreenState.Ready).shouldShowContinueButton,
+                onContinueGame = onContinueGame,
+                onStartNewEasyGame = onStartNewEasyGame,
+                onStartNewMediumGame = onStartNewMediumGame,
+                onStartNewHardGame = onStartNewHardGame
+            )
+        }
+    }
 }
 
 @Composable
@@ -48,39 +60,51 @@ private fun StartScreen(
     onStartNewMediumGame: () -> Unit,
     onStartNewHardGame: () -> Unit,
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxHeight()
+                .width(IntrinsicSize.Max),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-
-        if (shouldShowContinueButton) {
-            Button(
-                modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
-                onClick = onContinueGame
-            ) {
-                Text(text = stringResource(id = R.string.load_game_button))
+            if (shouldShowContinueButton) {
+                Button(
+                    onClick = onContinueGame,
+                    textRes = R.string.load_game_button
+                )
             }
+            Button(
+                onClick = onStartNewEasyGame,
+                textRes = R.string.difficulty_easy
+            )
+            Button(
+                onClick = onStartNewMediumGame,
+                textRes = R.string.difficulty_medium
+            )
+            Button(
+                onClick = onStartNewHardGame,
+                textRes = R.string.difficulty_hard
+            )
         }
-        Button(
-            modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
-            onClick = onStartNewEasyGame
-        ) {
-            Text(text = stringResource(id = R.string.difficulty_easy))
-        }
-        Button(
-            modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
-            onClick = onStartNewMediumGame
-        ) {
-            Text(text = stringResource(id = R.string.difficulty_medium))
-        }
-        Button(
-            modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
-            onClick = onStartNewHardGame
-        ) {
-            Text(text = stringResource(id = R.string.difficulty_hard))
-        }
+    }
+}
+
+@Composable
+private fun Button(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    @StringRes textRes: Int,
+) {
+    OutlinedButton(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        onClick = onClick
+    ) {
+        Text(text = stringResource(id = textRes))
     }
 }
 
