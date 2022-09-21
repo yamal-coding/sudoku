a74047cf16dbbd8b07b69bbac1e5f84559c53f65
yamal <yamal.coding@gmail.com>
2022-09-21 18:21:24 +0200

    using dark or light from material

--- app/src/main/java/com/yamal/sudoku/commons/ui/theme/SudokuTheme.kt
+++ app/src/main/java/com/yamal/sudoku/commons/ui/theme/SudokuTheme.kt
@@ -2,6 +2,8 @@ package com.yamal.sudoku.commons.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
@@ -16,7 +18,14 @@ fun SudokuTheme(
     CompositionLocalProvider(
         LocalSudokuColors provides colors
     ) {
-        MaterialTheme(content = content)
+        MaterialTheme(
+            colors = if (isDarkThemeActive) {
+                darkColors()
+            } else {
+                lightColors()
+            },
+            content = content
+        )
     }
 }
 
