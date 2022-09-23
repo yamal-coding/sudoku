9242b887e09d977f7c719b80c1b1015bdd194455
yamal <yamal.coding@gmail.com>
2022-09-23 14:36:01 +0200

    remove InitDarkMode dead code

--- app/src/androidTest/java/com/yamal/sudoku/test/SudokuTestBaseApplication.kt
+++ app/src/androidTest/java/com/yamal/sudoku/test/SudokuTestBaseApplication.kt
@@ -1,19 +1,5 @@
 package com.yamal.sudoku.test
 
 import com.yamal.sudoku.SudokuBaseApplication
-import com.yamal.sudoku.darkmode.InitDarkMode
-import dagger.hilt.InstallIn
-import dagger.hilt.android.EarlyEntryPoint
-import dagger.hilt.android.EarlyEntryPoints
-import dagger.hilt.components.SingletonComponent
 
-open class SudokuTestBaseApplication : SudokuBaseApplication() {
-    override val initDarkMode: InitDarkMode
-        get() = EarlyEntryPoints.get(this, TestEntryPoint::class.java).initDarkMode()
-
-    @EarlyEntryPoint
-    @InstallIn(SingletonComponent::class)
-    internal interface TestEntryPoint {
-        fun initDarkMode(): InitDarkMode
-    }
-}
+open class SudokuTestBaseApplication : SudokuBaseApplication()
--- app/src/main/java/com/yamal/sudoku/SudokuBaseApplication.kt
+++ app/src/main/java/com/yamal/sudoku/SudokuBaseApplication.kt
@@ -1,15 +1,5 @@
 package com.yamal.sudoku
 
 import android.app.Application
-import com.yamal.sudoku.darkmode.InitDarkMode
 
-abstract class SudokuBaseApplication : Application() {
-
-    abstract val initDarkMode: InitDarkMode
-
-    override fun onCreate() {
-        super.onCreate()
-
-        initDarkMode()
-    }
-}
+abstract class SudokuBaseApplication : Application()
