3d4c316aaae0ea598ecf69db232f35debdc96ed5
yamal <yamal.coding@gmail.com>
2025-04-07 18:15:32 +0200

    project build.gradle and settings.gradle to kts

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import com.android.build.gradle.api.AndroidBasePlugin
+
+buildscript {
+    dependencies {
+        classpath(libs.google.services.plugin)
+        classpath(libs.crashlytics.plugin)
+    }
+}
+
+plugins {
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.compose.compiler) apply false
+}
+
+detekt {
+    source.setFrom(rootProject.rootDir)
+    config.setFrom("$projectDir/detekt.yml")
+    buildUponDefaultConfig = true
+}
+
+subprojects {
+    val currentModule = name
+
+    if (currentModule == "app") {
+        val isReleaseBuildType = gradle.startParameter.taskRequests.toString().contains("Release")
+        if (isReleaseBuildType) {
+            apply(plugin = "com.google.gms.google-services")
+            apply(plugin = "com.google.firebase.crashlytics")
+        }
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
+
+tasks.register("ci") {
+    dependsOn("app:test", "app:lint", "app:assembleDebug", "detekt")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+import java.net.URI
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = URI.create("https://jitpack.io")
+        }
+    }
+}
+
+rootProject.name = "Sudoku"
+include(":app")
+
