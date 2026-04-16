94ab45a6e51b9e01cc898be375b52fad58d6ada8
yamal <yamal.coding@gmail.com>
2026-04-16 18:03:50 +0200

    revert version code being bumped

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.yamal.sudoku"
         minSdk =  libs.versions.min.sdk.get().toInt()
         targetSdk = libs.versions.sdk.version.get().toInt()
-        versionCode = 20251014
+        versionCode = 2025104
         versionName = "1.4.0"
 
         testInstrumentationRunner = "com.yamal.sudoku.test.di.HiltTestRunner"
